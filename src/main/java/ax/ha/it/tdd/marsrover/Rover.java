ae3d1918f386d783577f13a848a7ef432787386a
Christian Syväluoma <christian.syvaluoma@ha.ax>
2022-03-01 19:52:09 +0200

    testcase get rover position

--- src/main/java/ax/ha/it/tdd/marsrover/Rover.java
+++ src/main/java/ax/ha/it/tdd/marsrover/Rover.java
@@ -1,5 +1,17 @@
 package ax.ha.it.tdd.marsrover;
 
 public class Rover {
+    private int xPos;
+    private int yPos;
+    private String heading;
 
+    public Rover() {
+        this.xPos = 0;
+        this.yPos = 0;
+        this.heading = "N";
+    }
+
+    public String getPos() {
+        return "(" + xPos + "," + yPos + ")" + "(" + heading + ")";
+    }
 }
--- src/test/java/ax/ha/it/tdd/marsrover/RoverTest.java
+++ src/test/java/ax/ha/it/tdd/marsrover/RoverTest.java
@@ -6,5 +6,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RoverTest {
+    Rover rover;
 
+    @BeforeEach
+    void setUp() {
+        rover = new Rover();
+    }
+
+    @Test
+    void getRoverPos() {
+        assertEquals("(0,0)(N)",rover.getPos());
+    }
 }
